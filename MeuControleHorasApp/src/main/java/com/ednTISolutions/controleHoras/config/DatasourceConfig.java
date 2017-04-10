d192e91cf017e6cd1d572206b3a7e1ff7f252b0b
edneyRoldao <edneyroldao@gmail.com>
2017-04-09 23:16:15 -0300

    mac environment from intelij has been created

--- MeuControleHorasApp/src/main/java/com/ednTISolutions/controleHoras/config/DatasourceConfig.java
+++ MeuControleHorasApp/src/main/java/com/ednTISolutions/controleHoras/config/DatasourceConfig.java
@@ -1,7 +1,5 @@
 package com.ednTISolutions.controleHoras.config;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -9,14 +7,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -24,7 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:oracleDesenvConfig.properties")
+@PropertySource("classpath:database.properties")
 @EnableJpaRepositories("com.ednTISolutions.controleHoras.repositories")
 public class DatasourceConfig {
 
@@ -34,10 +28,10 @@ public class DatasourceConfig {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(this.env.getProperty("jdbc.driverClassName"));
-		dataSource.setUrl(this.env.getProperty("jdbc.url"));
-		dataSource.setUsername(this.env.getProperty("jdbc.username"));
-		dataSource.setPassword(this.env.getProperty("jdbc.password"));
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
 		
 		return dataSource;
 	}
@@ -62,32 +56,13 @@ public class DatasourceConfig {
 		
 		return transactionManager;
 	}
-	
-	@Bean
-	@Lazy(false)
-	public ResourceDatabasePopulator populateDatabase() throws SQLException {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("dataUpdates.sql"));
-		
-		Connection con = null;
-		
-		try {
-			con = DataSourceUtils.getConnection(dataSource());
-			populator.populate(con);
-		} finally {
-			if(con != null)
-				DataSourceUtils.releaseConnection(con, dataSource());
-		}
-		
-		return populator;
-	}
-	
+
 	private Properties hibernateProperties() {
 		Properties props = new Properties();
-		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl.auto", "update");
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");		
-		props.setProperty("hibernate.connection.driver_class", "oracle.jdbc.driver.OracleDriver");
+		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+		props.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		props.setProperty("hibernate.connection.driver_class", env.getProperty("hibernate.connection.driver_class"));
 		
 		return props;
 	}
