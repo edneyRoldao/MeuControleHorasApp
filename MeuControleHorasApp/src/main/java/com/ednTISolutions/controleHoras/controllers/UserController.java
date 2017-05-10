cd17018155b8014a731c15e2c5f449d759f6b3c5
edneyRoldao <edneyroldao@gmail.com>
2017-05-10 10:28:04 -0300

    I'm finishing create user screen

--- MeuControleHorasApp/src/main/java/com/ednTISolutions/controleHoras/controllers/UserController.java
+++ MeuControleHorasApp/src/main/java/com/ednTISolutions/controleHoras/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.ednTISolutions.controleHoras.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,12 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
+	@PostMapping
 	public ResponseEntity<User> createAndUpdateUser(@RequestBody User user) {
 		User newUser = service.createUser(user);
 		
-		if(newUser == null) {
+		if(newUser == null)
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-		}
 		
 		return ResponseEntity.ok(newUser);
 	}
--- MeuControleHorasApp/src/main/webapp/app/js/controllers/userController.js
+++ MeuControleHorasApp/src/main/webapp/app/js/controllers/userController.js
@@ -1,15 +1,39 @@
 // EdneyRoldao - 18/04/17
-function NewUserController(UserAPIService) {
+function NewUserController(UserAPIService, $location) {
     var _ctrl = this;
+    _ctrl.userAlreadyExists = false;
 
     _ctrl.createUser = function(user) {
+    	
+    	swal({
+    		  title: "Auto close alert!",
+    		  text: "I will close in 2 seconds.",
+    		  type: "success",
+    		  timer: 2000,
+    		  showConfirmButton: false
+    		});
+    	
+    	// http://t4t5.github.io/sweetalert/ documentation
+    	
+    	/*
         delete user.passRepeat;
+        _ctrl.userAlreadyExists = false;        	
 
         UserAPIService.createUser(user).success(function(data) {
-            console.log(data);
+            _ctrl.newUserForm.$setPristine();
+        	delete _ctrl.user;
+            $location.path("/login");
+            
         }).error(function(data, status) {
             console.log(status);
+            _ctrl.userAlreadyExists = true;
+            _ctrl.user.password = null;
+            _ctrl.user.passRepeat = null;
+            _ctrl.newUserForm.$setPristine();
+            $location.path("/cadastro");
+            
         });
+        */
     };
 
 }
