a8c15794312fdaec2d28c78a39bf0d15b7be3db5
edneyRoldao <edneyroldao@gmail.com>
2017-07-21 15:26:21 -0300

    new features

--- MeuControleHorasApp/src/main/webapp/app/js/controllers/profileController.js
+++ MeuControleHorasApp/src/main/webapp/app/js/controllers/profileController.js
@@ -1,9 +1,12 @@
 function ProfileController() {
     var ctrl = this;
-    ctrl.image = null;
-    ctrl.imageFileName = "";
-    ctrl.uploadme = {};
-    ctrl.uploadme.src = "";
+    ctrl.img = "";
+    ctrl.types = ["image/png", "image/jpeg", "image/gif"];
+
+    ctrl.test = function() {
+    	console.log("test ng change");
+    }
+
 }
 
 angular.module("meuControleHorasApp").controller("ProfileController", ProfileController);
