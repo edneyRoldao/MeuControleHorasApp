79271cd089b7de0541f2db5252821135566d8eac
edneyRoldao <edneyroldao@gmail.com>
2017-08-03 08:35:02 -0300

    at the edit profile screen were added behavior from a group of buttons

--- MeuControleHorasApp/src/main/webapp/app/js/controllers/profileController.js
+++ MeuControleHorasApp/src/main/webapp/app/js/controllers/profileController.js
@@ -1,10 +1,59 @@
 function ProfileController() {
     var ctrl = this;
+
     ctrl.img = "";
     ctrl.types = ["image/png", "image/jpeg", "image/gif"];
 
+    ctrl.btnFirstForm = {
+        'btn btn-success btn-sm': true,
+        'btn btn-white btn-sm': false
+    };
+
+    ctrl.btnSecondForm = {
+        'btn btn-success btn-sm': false,
+        'btn btn-white btn-sm': true
+    };
+
+    ctrl.btnThirdForm = {
+        'btn btn-success btn-sm': false,
+        'btn btn-white btn-sm': true
+    };
+
     ctrl.test = function() {
     	console.log("test ng change");
+    };
+
+    ctrl.changeForm = function(form) {
+        changeCssFromButton(form);
+    };
+
+    function changeCssFromButton (form) {
+        switch (form) {
+            case 1:
+                ctrl.btnFirstForm['btn btn-success btn-sm'] = true;
+                ctrl.btnFirstForm['btn btn-white btn-sm'] = false;
+                ctrl.btnSecondForm['btn btn-success btn-sm'] = false;
+                ctrl.btnSecondForm['btn btn-white btn-sm'] = true;
+                ctrl.btnThirdForm['btn btn-success btn-sm'] = false;
+                ctrl.btnThirdForm['btn btn-white btn-sm'] = true;
+                break;
+            case 2:
+                ctrl.btnSecondForm['btn btn-success btn-sm'] = true;
+                ctrl.btnSecondForm['btn btn-white btn-sm'] = false;
+                ctrl.btnFirstForm['btn btn-success btn-sm'] = false;
+                ctrl.btnFirstForm['btn btn-white btn-sm'] = true;
+                ctrl.btnThirdForm['btn btn-success btn-sm'] = false;
+                ctrl.btnThirdForm['btn btn-white btn-sm'] = true;
+                break;
+            case 3:
+                ctrl.btnThirdForm['btn btn-success btn-sm'] = true;
+                ctrl.btnThirdForm['btn btn-white btn-sm'] = false;
+                ctrl.btnFirstForm['btn btn-success btn-sm'] = false;
+                ctrl.btnFirstForm['btn btn-white btn-sm'] = true;
+                ctrl.btnSecondForm['btn btn-success btn-sm'] = false;
+                ctrl.btnSecondForm['btn btn-white btn-sm'] = true;
+                break;
+        }
     }
 
 }
